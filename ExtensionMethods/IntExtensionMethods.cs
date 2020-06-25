ba3a44e4e2c30d15519d3d7fef7cd45efbe8b677
Greg Doud <gpdoud@gmail.com>
2020-06-25 15:37:00 -0400

    Add project files.

---
+++ ExtensionMethods/IntExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods {
+    
+    public static class IntExtensionMethods {
+
+        public static int EvalFunc(this int x) {
+            return (x * x) + (3 * x) + 5;
+        }
+
+        public static long Squared(this long val) {
+            return val * val;
+        }
+
+        public static long Cubed(this long val) {
+            return val * val * val;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethods {
+    class Program {
+        static void Main(string[] args) {
+
+            long i = 3;
+            i.ToString();
+            var squaredValue = i.Squared();
+            var cubedValue = i.Cubed();
+            var magicValue = ((long)88).Squared();
+            var bigValue = ((long)5).Cubed().Squared();
+
+            var fx = 37.EvalFunc(); // 1485
+        }
+    }
+}
